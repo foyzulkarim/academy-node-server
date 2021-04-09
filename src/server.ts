cda97f6245008128c3560a3a3763d5c144f7a103
Foyzul Karim <foyzulkarim@gmail.com>
2021-04-10 01:25:40 +0800

    Added basic routes

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
-console.log('hello people.');
+import express, { Application } from "express";
+
+const init = (): Application => {
+    let app = express();
+
+    app.get('/', (req, res) => {
+        res.send(`thanks for requesting\n${req.method} at \n` + new Date());
+    })
+
+    app.use('/', (req, res) => {
+        res.send(`Other request:\n${req.method} at \n` + new Date());
+    });
+
+    return app;
+}
+
+const app = init();
+app.listen(3000, () => {
+    console.log('i am listening');
+});
