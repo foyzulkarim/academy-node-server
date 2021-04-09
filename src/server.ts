e6609e2ae84b82c0b57cbfe2cfd548fa5b797797
Foyzul Karim <foyzulkarim@gmail.com>
2021-04-10 01:57:49 +0800

    Added routes array and introduced dynamic routing

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,36 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
+
+
+export type Handler = (req: Request, res: Response) => void;
+
+interface IRoute {
+    http: string,
+    path: string,
+    handler: Handler,
+}
 
 const init = (): Application => {
     let app = express();
 
-    app.get('/', (req, res) => {
-        res.send(`thanks for requesting\n${req.method} at \n` + new Date());
+    let routes: IRoute[] = [
+        {
+            http: 'get',
+            path: '/',
+            handler: (req: Request, res: Response) => {
+                res.send(`thanks for requesting!!!\n${req.method} at \n` + new Date());
+            }
+        },
+        {
+            http: 'get',
+            path: '/dictionary',
+            handler: (req: Request, res: Response) => {
+                res.send(`it worked\n${req.method} at \n` + new Date());
+            }
+        }
+    ];
+
+    routes.forEach((route) => {
+        (app as any)[route.http](route.path, route.handler);
     })
 
     app.use('/', (req, res) => {
